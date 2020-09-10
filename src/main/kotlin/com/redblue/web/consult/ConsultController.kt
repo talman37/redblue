f8153067c19dd07bc4d9f7416b5c5b2cdc79e601
JungHamyung <jhm@mz.co.kr>
2020-09-10 09:11:53 +0900

    add resources

--- src/main/kotlin/com/redblue/web/consult/ConsultController.kt
+++ src/main/kotlin/com/redblue/web/consult/ConsultController.kt
@@ -1,7 +1,22 @@
 package com.redblue.web.consult
 
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
+@RequestMapping("/consult")
 class ConsultController {
+
+
+	@GetMapping
+	fun list(): String {
+		return "/consult/list"
+	}
+
+	@GetMapping("/add")
+	fun addForm(): String {
+		return "/consult/form"
+	}
+
 }
