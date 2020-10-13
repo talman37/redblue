e1e0ef52edc3621e62b976e6e910caeee3fd2a3b
JungHamyung <jhm@mz.co.kr>
2020-10-13 19:05:00 +0900

    법인등록 작업중

--- src/main/kotlin/com/redblue/web/company/CompanyRestController.kt
+++ src/main/kotlin/com/redblue/web/company/CompanyRestController.kt
@@ -1,16 +1,14 @@
 package com.redblue.web.company
 
 import com.redblue.security.core.annotation.CurrentUser
+import com.redblue.web.company.model.dto.CompanyCreateDto
 import com.redblue.web.company.service.CompanyExcelService
 import com.redblue.web.company.service.CompanyService
 import com.redblue.web.lawfirm.model.LawFirmUser
 import org.springframework.core.io.InputStreamResource
 import org.springframework.http.HttpHeaders
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.text.SimpleDateFormat
 
 @RestController
@@ -27,6 +25,13 @@ class CompanyRestController(
 	): ResponseEntity<String> {
 		val company = companyService.search(user.lawFirmId, q)
 		return ResponseEntity.ok("/company/${company.id}")
+	}
+
+	@PostMapping
+	fun save(
+		@RequestBody dto: CompanyCreateDto
+	) {
+
 	}
 
 	@GetMapping("/download/corporations.xlsx")
---
+++ src/main/kotlin/com/redblue/web/company/model/dto/CompanyCreateDto.kt
@@ -0,0 +1,107 @@
+package com.redblue.web.company.model.dto
+
+import com.redblue.web.company.model.Company
+import com.redblue.web.company.model.Executive
+import com.redblue.web.company.model.Stock
+import com.redblue.web.company.model.Stockholder
+import net.bytebuddy.utility.RandomString
+import java.util.*
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
+import javax.persistence.Temporal
+import javax.persistence.TemporalType
+
+data class CompanyCreateDto(
+
+	val id: String = "CM" + RandomString.make(30),
+
+	val baseCompanyId: String? = null,
+
+	val registerOffice: String? = null,
+
+	val registerNumber: Int? = null,
+
+	val companyNumber1: Int? = null,
+
+	val companyNumber2: Int? = null,
+
+	val companyName: String? = null,
+
+	val companyDivision: String?,
+
+	val companyManageNumber: String? = null,
+
+	val companyManageState: String? = null,
+
+	val companyState: String? = null,
+
+	@Enumerated(EnumType.STRING)
+	val displayCompanyType: Company.DisplayCompanyType? = Company.DisplayCompanyType.FRONT,
+
+	val companySubName: String? = null,
+
+	@Temporal(TemporalType.DATE)
+	val companyUpdatedAt: Date? = null,
+
+	val companyAddress: String? = null,
+
+	val companyPostalCode: String? = null,
+
+	@Temporal(TemporalType.DATE)
+	val companyAddressUpdatedAt: Date? = null,
+
+	val businessNumber: String? = null,
+
+	val businessType: String? = null,
+
+	val businessCondition: String? = null,
+
+	val deliveryPlace: String? = null,
+
+	val deliveryPlacePostalCode: String? = null,
+
+	val noticeWay: String? = null,
+
+	val noticeWayUpdatedAt: Date? = null,
+
+	val etc: String? = null,
+
+	val convertibleBond: String? = null,
+
+	val stockPurchaseOption: String? = null,
+
+	val companyFormationAt: Date? = null,
+
+	val registerRecordCreateReason: String? = null,
+
+	val registerRecordCreateAt: Date? = null,
+
+	val isHeadOfficeTransfer: Boolean? = null,
+
+	val headOfficeTransferAt: Date? = null,
+
+	val isDisband: Boolean? = null,
+
+	val disbandAt: Date? = null,
+
+	val disbandDeemedAt: Date? = null,
+
+	val isLiquidation: Boolean? = null,
+
+	val liquidationAt: Date? = null,
+
+	val isRegisterRecordClosure: Boolean? = null,
+
+	val registerRecordClosureAt: Date? = null,
+
+	val settlementMonth: Int? = null,
+
+	val recommender: String? = null,
+
+	var stock: Stock? = null,
+
+	var executives: List<Executive> = emptyList(),
+
+	var stockholders: List<Stockholder> = emptyList()
+
+)
