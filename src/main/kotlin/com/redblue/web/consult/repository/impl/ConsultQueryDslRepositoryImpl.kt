c6927f80be9142f6a9315a5cf2508e8ceefca1ed
JungHamyung <jhm@mz.co.kr>
2020-12-03 17:24:06 +0900

    상담페이지 임원 만료일 적용

--- src/main/kotlin/com/redblue/web/consult/repository/impl/ConsultQueryDslRepositoryImpl.kt
+++ src/main/kotlin/com/redblue/web/consult/repository/impl/ConsultQueryDslRepositoryImpl.kt
@@ -1,9 +1,12 @@
 package com.redblue.web.consult.repository.impl
 
 import com.querydsl.core.BooleanBuilder
+import com.querydsl.core.types.ExpressionUtils
 import com.querydsl.core.types.Projections
+import com.querydsl.jpa.JPAExpressions
 import com.querydsl.jpa.impl.JPAQueryFactory
 import com.redblue.web.company.model.QCompany
+import com.redblue.web.company.model.QExecutive
 import com.redblue.web.consult.model.Consult
 import com.redblue.web.consult.model.QConsult
 import com.redblue.web.consult.repository.ConsultQueryDslRepository
@@ -21,6 +24,7 @@ class ConsultQueryDslRepositoryImpl(
 	override fun findByLawFirmId(lawFirmId: String, pageable: Pageable): Page<Consult> {
 		val qc = QConsult.consult
 		val qp = QCompany.company
+		val qe = QExecutive.executive
 		val predicate = BooleanBuilder()
 		val query = jpaQueryFactory
 			.select(
@@ -30,6 +34,8 @@ class ConsultQueryDslRepositoryImpl(
 					qc.lawFirmId,
 					qc.companyId,
 					qp.companyName,
+					ExpressionUtils.`as`(JPAExpressions.select(qe.expiredAt.min())
+						.from(qe).where(qe.companyId.eq(qc.companyId)).limit(1), "expiredAt"),
 					qc.consultant,
 					qc.content,
 					qc.memo,
